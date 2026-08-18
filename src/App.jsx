cce73839204ede055f0d5a985800c5dc8dcd1887
Keith Kamau <keith.kamau2@student.moringaschool.com>
2026-08-18 14:15:37 +0300

    feat: centralize all styles into single global css file

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+// src/App.jsx
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Header } from "./components/common/Header";
+import { Footer } from "./components/common/Footer";
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+import DashboardPage from "./pages/DashboardPage";
+import CreateIncidentPage from "./pages/CreateIncidentPage";
+import IncidentDetailPage from "./pages/IncidentDetailPage";
+import AdminPage from "./pages/AdminPage";
+
+function App() {
+  return (
+    <Router>
+      <div className='app-container'>
+        <Header />
+        <main className='main-content'>
+          <Routes>
+            <Route path='/' element={<HomePage />} />
+            <Route path='/login' element={<LoginPage />} />
+            <Route path='/register' element={<RegisterPage />} />
+            <Route path='/dashboard' element={<DashboardPage />} />
+            <Route path='/incidents/create' element={<CreateIncidentPage />} />
+            <Route path='/incidents/:id' element={<IncidentDetailPage />} />
+            <Route path='/admin' element={<AdminPage />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
