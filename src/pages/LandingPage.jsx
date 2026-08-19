e3d6fe60586add11b3ec8735061e38811ff3052a
Keith Kamau <keith.kamau2@student.moringaschool.com>
2026-08-19 21:17:16 +0300

    feat: add landing page component

---
+++ src/pages/LandingPage.jsx
@@ -0,0 +1,9 @@
+// src/pages/LandingPage.jsx
+import React from "react";
+import { Splash } from "../components/auth/Splash";
+
+const LandingPage = () => {
+  return <Splash />;
+};
+
+export default LandingPage;
