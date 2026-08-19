31d5939bfadeb5c64d261802b2de723456cb5249
Keith Kamau <keith.kamau2@student.moringaschool.com>
2026-08-19 21:18:00 +0300

    feat: add login page component

---
+++ src/pages/LoginPage.jsx
@@ -0,0 +1,9 @@
+// src/pages/LoginPage.jsx
+import React from "react";
+import { Login } from "../components/auth/Login";
+
+const LoginPage = () => {
+  return <Login />;
+};
+
+export default LoginPage;
