3614d456c5a3066b624a00ac02355d0b2dcc3a74
Keith Kamau <keith.kamau2@student.moringaschool.com>
2026-08-29 02:05:38 +0300

    fix: remove non-existent global.css import

--- src/components/auth/Login.jsx
+++ src/components/auth/Login.jsx
@@ -3,7 +3,6 @@ import { useDispatch, useSelector } from "react-redux";
 import { Link, useNavigate } from "react-router-dom";
 import { loginUser, clearError } from "../../redux/slices/authSlice";
 import { validateEmail } from "../../utils/validators";
-import "../../styles/global.css";
 
 export const Login = () => {
     const dispatch = useDispatch();
